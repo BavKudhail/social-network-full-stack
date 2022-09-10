1f901c0798545efe6aeee26bb31eb263180e97c9
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-09-10 22:31:27 +0100

    building the first schema

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,22 @@ import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   orm.getMigrator().up();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
 
   app.get('/', (_, res) => {
     res.send('hello');
