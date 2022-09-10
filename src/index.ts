71c3db42cea0f69f847431ee1e7c50dc154a1164
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-09-10 18:44:19 +0100

    setting up database logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello world');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: 'redditDB',
+    user: 'postgres',
+    password: '19.january',
+    debug: !__prod__,
+    type: 'postgresql',
+  });
+};
+
+main();
